ee21fe53ef6da9d13738aaa19a8c08ae0d3008dc
Udit Kumar <kudit223@gmail.com>
2026-03-05 00:18:57 +0530

    updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,26 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+
+  const fruits = ["apple", "banana", "cherry", "date", "elderberry", "fig"];
+  const [displayFruits, setDisplayFruits] = useState(fruits);
+
+  const handleChange=(e)=>{
+      const searchFruits=fruits.filter((item)=>item.includes(e.target.value));
+      setDisplayFruits(searchFruits);
+  }
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h1>Search item</h1>
+      <input onChange={handleChange} ></input>
+      <ul>
+        {displayFruits.map((item,index) => {
+            return <li key={index}>{item}</li>
+        })}
+      </ul>
+
     </div>
   )
 }
